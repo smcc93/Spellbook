cc452d4831f5267dc358f0fecc0eacc1f2dce077
Stewart McCalley <smccalle@iu.edu>
2018-06-05 11:39:43 -0400

    Final Form

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('testing')
+const headChange = document.querySelector('h2');
+
+const spellName = document.querySelector('h3');
+
+const inputOne = document.querySelector('#school')
+
+const inputTwo = document.querySelector('#spell');
+
+const form = document.querySelector('form');
+form.addEventListener('submit', change);
+
+function change(ev){
+    ev.preventDefault()
+    headChange.textContent = inputOne.value;
+
+    spellName.textContent = inputTwo.value;
+
+}
