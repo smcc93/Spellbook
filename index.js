f7ebc6d8fc24ed4da3722a9615589ad523863dc0
Stewart McCalley <smccalle@iu.edu>
2018-06-06 11:43:08 -0400

    nest spells into spell type

--- index.js
+++ index.js
@@ -2,31 +2,24 @@ const form = document.querySelector('form');
 
 function add(ev){
     ev.preventDefault()
-    
-    
+        
     const newSpell = ev.target;
     const addSpell = newSpell.spellName.value;
+    const addType = newSpell.spellType.value;
 
     const dvList = document.querySelector('#spellList');
     const spellList= document.createElement('li');
+
+    const dvtypeList = document.querySelector('#typeList');
+    const typeList = document.createElement('li');
     spellList.appendChild(document.createTextNode(addSpell));
+    spellList.appendChild(document.createTextNode(addType));
     dvList.appendChild(spellList);
+    dvtypeList.appendChild(typeList);
     newSpell.reset();
+    
 }
 
 form.addEventListener('submit', add);
 
-function TypeAdd(ev){
-    ev.preventDefault()
-
-    const newType = ev.target;
-    const addType = newType.spellType.value;
-
-    const dvTypeList = document.querySelector('#typeList');
-    const typeList= document.createElement('li');
-    typeList.appendChild(document.createTextNode(addType));
-    dvTypeList.appendChild(typeList);
-
-}
-form.addEventListener('submit', TypeAdd);
  
