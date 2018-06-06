0ff7f9e42d7a8e56b8c63346e718bb627574deda
Stewart McCalley <smccalle@iu.edu>
2018-06-06 11:19:18 -0400

    add spells to a list, change the background, remove head change function

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const headChange = document.querySelector('h2');
-
-const spellName = document.querySelector('h3');
-
-const inputOne = document.querySelector('#school')
-
-const inputTwo = document.querySelector('#spell');
-
 const form = document.querySelector('form');
-form.addEventListener('submit', change);
 
-function change(ev){
+function add(ev){
     ev.preventDefault()
-    headChange.textContent = inputOne.value;
+    
+    
+    const newSpell = ev.target;
+    const addSpell = newSpell.spellName.value;
 
-    spellName.textContent = inputTwo.value;
+    const dvList = document.querySelector('#spellList');
+    const spellList= document.createElement('li');
+    spellList.appendChild(document.createTextNode(addSpell));
+    dvList.appendChild(spellList);
+    newSpell.reset();
+}
+
+form.addEventListener('submit', add);
+
+function TypeAdd(ev){
+    ev.preventDefault()
+
+    const newType = ev.target;
+    const addType = newType.spellType.value;
+
+    const dvTypeList = document.querySelector('#typeList');
+    const typeList= document.createElement('li');
+    typeList.appendChild(document.createTextNode(addType));
+    dvTypeList.appendChild(typeList);
 
 }
+form.addEventListener('submit', TypeAdd);
+ 
