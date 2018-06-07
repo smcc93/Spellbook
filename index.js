ad53ce6fe85a4d4caa88726434438adbd202c189
Stewart McCalley <smccalle@iu.edu>
2018-06-06 21:37:53 -0400

    updated javascript for streamlined functions

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-const form = document.querySelector('form');
+const book = {
 
-function add(ev){
-    ev.preventDefault()
-        
-    const newSpell = ev.target;
-    const addSpell = newSpell.spellName.value;
-    const addType = newSpell.spellType.value;
+    init: function() {
+  
+     const form = document.querySelector('form')
+  
+     form.addEventListener('submit', (ev) => {
+  
+     ev.preventDefault()
+  
+     this.handleSubmit(ev)
+  
+     })
+  
+    },
+    renderProperty: function(name, value) {
 
-    const dvList = document.querySelector('#spellList');
-    const spellList= document.createElement('li');
+     const el = document.createElement('span')
 
-    const dvtypeList = document.querySelector('#typeList');
-    const typeList = document.createElement('li');
+     el.classList.add(name)
+
+     el.textContent = value
+
+     el.setAttribute('title', value)
+
+     return el
+
+    },
+    renderItem: function(spell) { 
+      
+      const properties = Object.keys(spell) 
+       
+      const childElements = properties.map((prop) => { 
     
-    typeList.appendChild(document.createTextNode(addType));
-    spellList.appendChild(document.createTextNode(addSpell));
-    dvList.appendChild(spellList);
-    dvtypeList.appendChild(typeList);
-    newSpell.reset();
+      return this.renderProperty(prop, spell[prop]) 
+    
+     }) 
+
+     const spellList= document.createElement('li');
+     childElements.forEach(function(el) { 
+
+       spellList.appendChild(el) 
+      })
+      return item 
+    },     
+    handleSubmit: function(ev) { 
+
+     const f = ev.target 
+       
+     const spell = { 
+    
+     name: f.spellName.value, 
+    
+     spellType: f.spellType.value, 
+     } 
+    
+     const item = this.renderItem(spell) 
+     const list = document.querySelector('#spellList') 
+    
+     list.appendChild(item) 
+
+     f.reset() 
+    }, 
     
 }
 
-form.addEventListener('submit', add);
+
 
  
